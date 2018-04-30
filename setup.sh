70363470c12413d1072ab67461a9faa274b6e909
oleg <oleg@d64p2.myreal.space>
2018-04-30 14:03:52 +0300

    modified the setup to run from forum.charg.io

--- setup.sh
+++ setup.sh
@@ -1,33 +1,21 @@
 
-# opkg update && opkg install ca-bundle ca-certificates && wget https://raw.githubusercontent.com/ChargCoinLLC/NODE/master/setup.sh -q -O /tmp/setup.sh.tmp && tr -d '\r' < /tmp/setup.sh.tmp > /tmp/setup.sh && chmod 755 /tmp/setup.sh && /tmp/setup.sh
+# opkg update && opkg install ca-bundle ca-certificates && wget https://forum.charg.io/setup.sh -q -O /tmp/setup.sh.tmp && tr -d '\r' < /tmp/setup.sh.tmp > /tmp/setup.sh && chmod 755 /tmp/setup.sh && /tmp/setup.sh
 
-setup_host=https://raw.githubusercontent.com/ChargCoinLLC/NODE/master/
-
-mkdir -p /www/cgi-bin
-mkdir -p /www/charg
-printf  "."
-wget $setup_host/usr/bin/init_service -q -O /tmp/script.tmp && tr -d '\r' < /tmp/script.tmp > /usr/bin/init_service && chmod 755 /usr/bin/init_service
-printf  "."
-wget $setup_host/usr/bin/charg_service -q -O /tmp/script.tmp && tr -d '\r' < /tmp/script.tmp > /usr/bin/charg_service && chmod 755 /usr/bin/charg_service
-printf  "."
-wget $setup_host/usr/bin/format_sd -q -O /tmp/script.tmp && tr -d '\r' < /tmp/script.tmp > /usr/bin/format_sd && chmod 755 /usr/bin/format_sd
-printf  "."
-wget $setup_host/www/cgi-bin/cgi-chg -q -O /tmp/script.tmp && tr -d '\r' < /tmp/script.tmp > /www/cgi-bin/cgi-chg && chmod 755 /www/cgi-bin/cgi-chg
-printf  "."
-wget $setup_host/www/charg/index.html -q -O /tmp/script.tmp && tr -d '\r' < /tmp/script.tmp > /www/charg/index.html
-printf  "."
-wget $setup_host/www/charg/login.html -q -O /tmp/script.tmp && tr -d '\r' < /tmp/script.tmp > /www/charg/login.html
-printf  "."
-wget $setup_host/www/charg/setup.html -q -O /tmp/script.tmp && tr -d '\r' < /tmp/script.tmp > /www/charg/setup.html
-printf  "."
-wget $setup_host/usr/bin/db_call -q -O /tmp/script.tmp && cp /tmp/script.tmp /usr/bin/db_call && chmod 755 /usr/bin/db_call
-printf  "."
+setup_host=https://forum.charg.io/
+wget $setup_host/node.tar.gz -q -O /tmp/node.tar.gz
+cd /
+tar xzf /tmp/node.tar.gz 
+chmod 755 /usr/bin/init_service
+chmod 755 /usr/bin/charg_service
+chmod 755 /usr/bin/db_call
+chmod 755 /usr/bin/format_sd
+chmod 755 /www/cgi-bin/cgi-chg
 
 echo "
 config options station
         option address ''
         option state 'idle'
-		
+	
 config options client
         option address ''
         option state ''
@@ -39,7 +27,6 @@ config options service
         option switch_port 11
         option protocol secure
         option debug 0
-	
 " > /etc/config/node
 
 echo "
@@ -53,3 +40,6 @@ if [ "$(opkg list-installed | grep swap-utils)" = "" ]; then
 fi
 
 echo " Done!"
+	
+
+
